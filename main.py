35fe1888b3fb1d851c00e9ef28df11444fb28ef8
gavinwalters <66943523+gavinwalters@users.noreply.github.com>
2023-10-31 21:36:03 -0400

    add window size to fix headless error

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ def initDriver(community):
     options = Options()
     prefs = {'download.default_directory' : '/Users/gavin/Documents/Townfolio data/' + community}
     options.add_experimental_option('prefs', prefs)
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--start-maximized")
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     # stop browser from closing after run
